b87ee55700bdd3c01940743d30ba41b699acf243
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-07-28 17:49:50 +1000

    delete node with a given value using the `.deleteItem(value)` method (#9)
    * write tests in index.mjs
    
    * add comment for clarity
    
    * recursively call deleteFromBST helper function in deleteItem method. successfully delete leaf nodes
    
    * successfully delete node with two children (324 node) but there is still the issue of deleting 8 (root node of the whole tree) and 4
    
    * fix: update incorrect property used. add console.log statement for testing/debugging. successfully copy over next largest node.
    
    however, that next largest node has not itself been deleted
    
    * fix: delete the next largest value
    
    i was calling the incorrect value. we should be recursively calling the next largest value (.data) at the end, in the scenario where the value for deletion has two children. after we replace the deleted node with the next largest, we delete the next largest
    
    * remove all console log statements used in testing/debugging except for the statement that advises what the value being deleted is for clarity when running

--- createTree.mjs
+++ createTree.mjs
@@ -24,23 +24,67 @@ const createTree = (inputArr) => {
   }
 
   function insert(value) {
-    const insertIntoBST = (rootNode, value) => {
-      if (rootNode === null) return createNode(value);
+    const insertIntoBST = (rootNode, val) => {
+      // base case: add node with value if tree empty OR space available on leaf
+      if (rootNode === null) return createNode(val);
 
       // no duplicates allowed
-      if (value === rootNode.data)
+      if (val === rootNode.data)
         return console.error(
-          `Cannot insert value, ${value}, as it already exists in the binary search tree.`
+          `Cannot insert value, ${val}, as it already exists in the binary search tree.`
         );
 
-      if (value > rootNode.data)
-        rootNode.right = insertIntoBST(rootNode.right, value);
-      else rootNode.left = insertIntoBST(rootNode.left, value);
+      if (val > rootNode.data)
+        rootNode.right = insertIntoBST(rootNode.right, val);
+      else rootNode.left = insertIntoBST(rootNode.left, val);
 
       return rootNode;
     };
 
     return insertIntoBST(root, value);
+  }
+
+  function deleteItem(value) {
+    console.log(`
+Deleting node with value = ${value}
+    `);
+    const getNextLargestData = (node) => {
+      let res = node.data;
+
+      // stop when there is no smaller value (no left subtree)
+      while (node.left !== null) {
+        res = node.left.data;
+        node = node.left; // traverse down and left
+      }
+
+      return res;
+    };
+
+    const deleteFromBST = (rootNode, val) => {
+      if (rootNode === null) return rootNode;
+
+      if (val > rootNode.data)
+        rootNode.right = deleteFromBST(rootNode.right, val);
+      else if (val < rootNode.data)
+        rootNode.left = deleteFromBST(rootNode.left, val);
+      // if the node value matches, we've found the node for deletion
+      else {
+        // node with only one child or no child (is leaf node)
+        if (rootNode.left === null) return rootNode.right;
+        else if (rootNode.right === null) return rootNode.left;
+
+        // node with 2 children -> get next largest in right subtree
+        const nextLargest = getNextLargestData(rootNode.right);
+        rootNode.data = nextLargest;
+
+        // delete that next largest value
+        rootNode.right = deleteFromBST(rootNode.right, nextLargest);
+      }
+
+      return rootNode;
+    };
+
+    return deleteFromBST(root, value);
   }
 
   function prettyPrint(node = root, prefix = "", isLeft = true) {
@@ -54,7 +98,7 @@ const createTree = (inputArr) => {
     }
   }
 
-  return { buildTree, insert, prettyPrint };
+  return { buildTree, deleteItem, insert, prettyPrint };
 };
 
 export default createTree;
