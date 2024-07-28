e3881b2b314bb4cfdcafc445c29afb2118e23ced
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-28 12:12:47 +1000

    a node has the data, its left child, and right child. these properties are returned. fixes #1
    no methods required at this stage

--- createNode.mjs
+++ createNode.mjs
@@ -1,5 +1,9 @@
-const createNode = () => {
-  console.log("hello");
+const createNode = (d) => {
+  const data = d || null;
+  const left = null;
+  const right = null;
+
+  return { data, left, right };
 };
 
 export default createNode;
