6675ced220e34d9d34b232169c73d97f5b4452a3
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-28 12:18:32 +1000

    add test data. fix method call for buildTree so that it can be called before declared, and is actually returned.

--- createTree.mjs
+++ createTree.mjs
@@ -1,9 +1,11 @@
 const createTree = (arr) => {
   const root = buildTree(arr);
 
-  const buildTree = (arr) => {
-    return;
-  };
+  function buildTree(inputArr) {
+    return inputArr;
+  }
+
+  return { buildTree };
 };
 
 export default createTree;
--- index.mjs
+++ index.mjs
@@ -1,3 +1,6 @@
 import createTree from "./createTree.mjs";
 
-createTree();
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = createTree(testArray);
+
+tree.buildTree(testArray);
