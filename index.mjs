a4d66a7be25f90baadbe4193c1c5a46e9dc3b936
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-28 14:51:58 +1000

    update tests

--- index.mjs
+++ index.mjs
@@ -3,5 +3,15 @@ import createTree from "./createTree.mjs";
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = createTree(testArray);
 
-console.log(tree.buildTree());
+tree.buildTree();
+tree.prettyPrint();
+
+// inserting
+console.log(`
+Inserting...
+`);
+tree.insert(69);
+tree.insert(420);
+tree.insert(-6969);
+
 tree.prettyPrint();
