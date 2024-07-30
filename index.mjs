947dc025aab78b7627c1e977374f19b8c38b82fc
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-30 10:32:54 +1000

    generate a binary search tree from the test array of random numbers from 0 to 100

--- index.mjs
+++ index.mjs
@@ -15,14 +15,11 @@ const generateArrOfRandomNumbers = (numberOfElements = 7) => {
   return res;
 };
 
-console.log(generateArrOfRandomNumbers());
+const testArray = generateArrOfRandomNumbers(10)
 
-// const testArray = [1, 2, 7, 4, 23, 8, 9, 10, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-// const tree = createTree(testArray);
-
-// tree.buildTree(testArray);
-// tree.prettyPrint();
+const tree = createTree(testArray);
+tree.buildTree(testArray);
+tree.prettyPrint();
 // console.log(tree.isBalanced()); // expected true
 
 // // create an unbalanced tree by removing two levels on the right-most subtree
