41fc3c8cc658d3f3ed01771e8100d5c8c6fa30dc
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-30 10:31:40 +1000

    generate an array of random integers between 0 and 100 for testing
    make parameter the number of elements required, which by default is 7 because this would, create a tree of height 2 which likely is illustrative.

--- index.mjs
+++ index.mjs
@@ -1,22 +1,39 @@
+// driver script
+
 import createTree from "./createTree.mjs";
 
-const testArray = [1, 2, 7, 4, 23, 8, 9, 10, 4, 3, 5, 7, 9, 67, 6345, 324];
+const generateArrOfRandomNumbers = (numberOfElements = 7) => {
+  const MIN_NUMBER = 0;
+  const MAX_NUMBER = 100;
 
-const tree = createTree(testArray);
+  const res = [];
+  for (let count = 1; count <= numberOfElements; count++)
+    res.push(
+      Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER)
+    );
 
-tree.buildTree(testArray);
-tree.prettyPrint();
-console.log(tree.isBalanced()); // expected true
+  return res;
+};
 
-// create an unbalanced tree by removing two levels on the right-most subtree
-tree.deleteItem(6345);
-tree.deleteItem(67);
-tree.deleteItem(324);
+console.log(generateArrOfRandomNumbers());
 
-tree.prettyPrint();
-console.log(tree.isBalanced()); // expected false
+// const testArray = [1, 2, 7, 4, 23, 8, 9, 10, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-// rebalancing
-tree.rebalance();
-tree.prettyPrint();
-console.log(tree.isBalanced()); // expected true
+// const tree = createTree(testArray);
+
+// tree.buildTree(testArray);
+// tree.prettyPrint();
+// console.log(tree.isBalanced()); // expected true
+
+// // create an unbalanced tree by removing two levels on the right-most subtree
+// tree.deleteItem(6345);
+// tree.deleteItem(67);
+// tree.deleteItem(324);
+
+// tree.prettyPrint();
+// console.log(tree.isBalanced()); // expected false
+
+// // rebalancing
+// tree.rebalance();
+// tree.prettyPrint();
+// console.log(tree.isBalanced()); // expected true
